380b18f9c375e8aa67429fd47abae1217e3bb559
hcmutemimi <hoaimiqng@gmail.com>
2021-10-16 23:00:18 +0700

    :tada: set up project
    set up project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'normalize.css'
+import 'src/assets/styles/global.scss'
+function App() {
+  return (
+    <div className="App">
+        hihi
+    </div>
+  )
+}
+
+export default App
